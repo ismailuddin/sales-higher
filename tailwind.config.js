5bafc98ca50d303ebe6541af6f6c950c94187a0b
ismailuddin <ismail.sameeuddin@gmail.com>
2022-12-05 21:00:57 +0000

    Setup website

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#0351c1'
+      }
+    },
+  },
+  plugins: [],
+}
